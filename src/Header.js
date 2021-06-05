a8be94b02ef6faebade7a7470c6dec1079c19fbb
gulxarahmad <gulloahmads@gmail.com>
2021-06-05 17:22:08 +0500

    Menu added

--- src/Header.js
+++ src/Header.js
@@ -2,10 +2,33 @@ function Header() {
 
  return(
      <>
-     <ul>
+     <nav className="navbar navbar-expand-lg navbar-light bg-light">
+  <div className="container-fluid">
+    <a className="navbar-brand" href="/">Home</a>
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
+    </button>
+    <div className="collapse navbar-collapse" id="navbarNavDropdown">
+      <ul className="navbar-nav">
+        <li className="nav-item">
+          <a className="nav-link" href="/posts">Posts</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="/users">Users</a>
+        </li>
+        
+      </ul>
+      <form className="d-flex">
+        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
+        <button className="btn btn-outline-success" type="submit">Search</button>
+      </form>
+    </div>
+  </div>
+</nav>
+     {/* <ul>
          <li> <a href='/posts'>Posts</a></li>
          <li> <a href='/users'>Users</a></li>
-         </ul>
+         </ul> */}
      </>
  );
     
